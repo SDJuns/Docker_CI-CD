fea3a6a2c3b693427b68ffa81a3d87ed0fdca0a2
SDJuns <51818069+SDJuns@users.noreply.github.com>
2025-05-27 23:56:42 +0900

    Delete app directory

--- app/api/main.py
+++
@@ -1,5 +0,0 @@
-from fastapi import FastAPI
-from .predict import router as predict_router
-
-app = FastAPI()
-app.include_router(predict_router)
--- app/model/model.py
+++
@@ -1,8 +0,0 @@
-import torch.nn as nn
-from torchvision import models
-
-def get_model():
-    model = models.efficientnet_b0(pretrained=True)
-    num_features = model.classifier[1].in_features
-    model.classifier[1] = nn.Linear(num_features, 4)
-    return model
